e91259c09eb78fda96c024b6f8b94f51890554a0
yassin772 <150456746+yassin772@users.noreply.github.com>
2023-12-03 05:09:52 +0100

    Create Jeu.py

---
+++ Jeu.py
@@ -0,0 +1,22 @@
+import random
+
+def guess_the_number():
+    # Générer un nombre aléatoire entre 1 et 10
+    secret_number = random.randint(1, 10)
+    
+    print("Bienvenue dans le jeu Guess the Number!")
+    print("Devinez le nombre entre 1 et 10.")
+
+    while True:
+        # Demander à l'utilisateur de deviner le nombre
+        user_guess = int(input("Entrez votre guess : "))
+
+        # Vérifier si le guess est correct
+        if user_guess == secret_number:
+            print("Félicitations ! Vous avez deviné le bon nombre.")
+            break
+        else:
+            print("Désolé, ce n'est pas le bon nombre. Essayez à nouveau.")
+
+if __name__ == "__main__":
+    guess_the_number()
